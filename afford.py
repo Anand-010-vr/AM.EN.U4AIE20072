008eebae3405734e144c0657c38df78b372211ac
Anand-010-vr <74124160+Anand-010-vr@users.noreply.github.com>
2023-08-19 11:17:40 +0530

    Add files via upload

---
+++ afford.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+
+
+
+import requests
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/numbers', methods=['GET'])
+def get_numbers():
+
+
+    urls = request.args.getlist('url')
+
+
+
+    if not urls:
+        return jsonify({"error": "No URLs provided"}), 400
+
+    result = []
+
+
+
+
+    for url in urls:
+        try:
+            response = requests.get(url)
+            data = response.json()
+            numbers = data.get("numbers")
+
+
+
+            if numbers and isinstance(numbers, list):
+                result.extend(numbers)
+        except Exception as e:
+            print(f"Error fetching data from {url}: {str(e)}")
+
+    return jsonify({"numbers": result})
+
+
+
+if __name__ == '_main_':
+    app.run(host='0.0.0.0', port=8008)
